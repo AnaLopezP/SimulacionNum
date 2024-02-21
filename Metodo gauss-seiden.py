e9071340d3ae6ceb4467ee4e8db4668bed4fdf13
Ana López <anamuyamiga@gmail.com>
2024-02-21 17:16:25 +0100

    miau

--- Metodo gauss-seiden.py
+++ Metodo gauss-seiden.py
@@ -14,3 +14,25 @@ k = d-c/M
 # Inicializar la matriz
 w = [[]]
 
+# Creamos la matriz
+for i in range(N):
+    for j in range(M):
+        w[i][j] = 0
+
+def funcion(i, j):
+    return None
+
+# Condiciones de frontera
+for i in range(1, N):
+    w[i][0] = None
+    w[i][M] = None
+    
+for j in range(1, M):
+    w[0][j] = None
+    w[N][j] = None
+    
+#recorremos los puntos interiores de la malla
+for k in range(100): # iteramos 100 veces. de momento, luego pondremos condiciones de parada
+    for i in range(1, N):
+        for j in range(1, M):
+            w[i][j] = (h**2+k**2)**2*(k**2*(w[i+1][j]+w[i-1][j]) + h**2*(w[i][j+1]+w[i][j-1]))/(2*(h**2+k**2)**2)
