42b9052931b2499566360b361b0063684a05c984
Ana López <anamuyamiga@gmail.com>
2024-02-21 17:04:42 +0100

    comienzoo

---
+++ Metodo gauss-seiden.py
@@ -0,0 +1,16 @@
+# a, b, c y d son las esquinas de la malla
+a = float(input("Ingrese el valor de a: "))
+b = float(input("Ingrese el valor de b: "))
+c = float(input("Ingrese el valor de c: "))
+d = float(input("Ingrese el valor de d: "))
+# Los límites de i y j
+N = float(input("Ingrese el valor de N: "))
+M = float(input("Ingrese el valor de M: "))
+
+# Los pasos de i y j
+h = b-a/N
+k = d-c/M
+
+# Inicializar la matriz
+w = [[]]
+
