5cca6fb0e02ec38a8a511d52fa57a7e70c6e7171
Ana López <anamuyamiga@gmail.com>
2024-02-28 17:20:07 +0100

    no entiendo por que no sale

--- Metodo gauss-seiden.py
+++ Metodo gauss-seiden.py
@@ -22,18 +22,18 @@ def funcion(i, j):
 # Condiciones de frontera
 for i in range(1, N):
     w[i][0] = 0
-    w[i][M] = 0
+    w[i][M] = (a + i*h)**2
     
 for j in range(1, M):
-    w[0][j] = 0
+    w[0][j] = 1 - (c + j*k)**2
     w[N][j] = 1
     
 #recorremos los puntos interiores de la malla
 for rana in range(100): # iteramos 100 veces. de momento, luego pondremos condiciones de parada
     for i in range(1, N):
         for j in range(1, M):
-            w[j][i] = (k**2 * (w[j+1][i] + w[j-1][i]) + h**2 * (w[j][i+1] + w[j][i-1]) - h**2 * k**2 * funcion(j, i)) / (2*(h**2 + k**2))
-print(w[j][i])
+            w[i][j] = (k**2 * (w[i+1][j] + w[i-1][j]) + h**2 * (w[i][j+1] + w[i][j-1]) - h**2 * k**2 * funcion(i, j)) / (2*(h**2 + k**2))
+print(w[i][j])
 # Mostrar la solución
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
@@ -78,3 +78,4 @@ u(0, y)= u(x, 0) = u(x, 1) = 0
 u(1, y) = 1'''#el señor dice que la u es la w, y que entomces en la f tenemos que poner w y la w original hay que cambiarla 
 
 # el laplaciano es la divergencia del gradiente. el gradiente es el triangulo para abajo. osea que la A = traángulo al cuadrado
+#nos va a preguntar por un análisis de la gráfica
