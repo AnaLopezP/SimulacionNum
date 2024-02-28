b6443b3e14034cb4451e6a79c9050da5e403fa72
Ana López <anamuyamiga@gmail.com>
2024-02-28 18:07:54 +0100

    lskdñaskñalkd

--- Helmholtz metodo gauss-seiden.py
+++ Helmholtz metodo gauss-seiden.py
@@ -10,6 +10,8 @@ d = np.pi
 N = int(input("Ingrese el valor de N: "))
 M = int(input("Ingrese el valor de M: "))
 
+# Valor de lambda 
+l = int(input("Ingrese el valor de lambda: "))
 
 # Los pasos de i y j
 h = (b-a)/N
@@ -30,14 +32,14 @@ for i in range(1, N):
     w[i][M] = 0
     
 for j in range(1, M):
-    w[0][j] = c + k*j
-    w[N][j] = c + k*j
+    w[0][j] = 0
+    w[N][j] = 1
     
 #recorremos los puntos interiores de la malla
 for rana in range(100): # iteramos 100 veces. de momento, luego pondremos condiciones de parada
     for i in range(1, N):
         for j in range(1, M):
-            w[i][j] = (k**2 * (w[i+1][j] + w[i-1][j]) + h**2 * (w[i][j+1] + w[i][j-1]) - h**2 * k**2 * funcion(i, j)) / (2*(h**2 + k**2))
+            w[i][j] = (k**2 * (w[i+1][j] + w[i-1][j]) + h**2 * (w[i][j+1] + w[i][j-1]) - h**2 * k**2 * funcion(i, j)) / (2*(h**2 + k**2) - (h*k*l)**2)
 print(w[i][j])
 # Mostrar la solución
 import matplotlib.pyplot as plt
@@ -66,3 +68,15 @@ ax.set_zlabel('Z')
 
 # Muestra la gráfica
 plt.show()
+'''el codigo es igual pero sale un -h**2*k**2*l**2 en el denominador de la ecuacion de relajacion'''
+''' 
+ecuacion de Helmholtz
+u_xx + u_yy + lambda*u = f(x,y)
+donde f(x,y) = 0
+u(0,y) = u(x,0) = u(x, 1) = 0
+u(1, y) = 1
+vamos a hacer 3 casos
+1. lambda = 1
+2. lambda = 300
+3. lambda = 1000
+'''
